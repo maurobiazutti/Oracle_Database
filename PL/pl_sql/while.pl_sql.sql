eba0e46661c3da44c345484dfe664437a7b0ca85
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-05 19:15:24 -0300

    while

---
+++ PL/pl_sql/while.pl_sql.sql
@@ -0,0 +1,13 @@
+-- WHILE LOOP
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(38) :=  1;
+  vLimite  NUMBER(38) := &pLimite;
+BEGIN
+WHILE  vNumero <= vLimite LOOP
+    DBMS_OUTPUT.PUT_LINE('Numero =  ' || to_char(vNumero));
+    vNumero := vNUmero + 1;
+END LOOP;
+END;
