eec4f0858dfcb46948419e92ea0d0fdcd1a07cb8
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-14 19:27:30 -0300

    resumo_02

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -11,3 +11,49 @@ Ele remove as duplicatas dos resultados da consulta,
 exibindo apenas um registro para cada valor único encontrado*/
 SELECT DISTINCT department_id
 FROM employees;
+
+--WHERE
+--Vai trazer todos os department_id = 90
+SELECT employee_id, last_name, job_id, department_id
+FROM employees
+WHERE department_id = 90;
+
+--Vai buscar os salarios entre 8000 e 15000
+SELECT first_name, salary
+FROM employees
+WHERE salary BETWEEN 8000 and 15000;
+
+--Busca uma lista de valores utilizando o operador IN
+SELECT first_name, salary, job_id
+FROM employees
+WHERE job_id IN ('IT_PROG', 'FI_ACCOUNT', 'SA_REP');
+
+--Busca com WHERE LIKE
+SELECT first_name ,last_name
+FROM employees
+WHERE first_name LiKE '%ha%'--Busca todas as palavras que tenha "ha"
+AND last_name LIKE '%ll%';--"AND -> E" Todos q contenha "ll" no last_name
+
+WHERE first_name LiKE 'I%';--Começa com "I" 
+WHERE first_name LiKE 'ar%';--Termina com "ar"
+WHERE first_name LiKE 'I%l'; --Começa com "I" e termina com "l"
+
+--WHERE NULL
+WHERE manager_id IS NULL; -- Para busca "NULL" na coluna
+
+--WHERE AND EXIBE QUANDO AS DUAS CONDIÇÕES FOREM TRUE
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary >= 5000
+AND job_id = 'IT_PROG';--Busca pelos salarios MAIOR OU IGUAL A 5000 E que sejam IT_PROG
+
+--WHERE OR Vai trazer todos que atenda pelo menos UM dos requisitos
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary >= 5000
+OR job_id = 'IT_PROG';
+
+--WHERE NOT
+SELECT employee_id, last_name ,salary, job_id
+FROM employees
+WHERE job_id NOT IN ('IT_PROG', 'FI_ACCOUNT', 'SA_REP'); --Vai Trazer todos que não estão na lista.
