4eeabd7a1c33bb0c1adf3d942044cd43ecf3ea40
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-20 17:35:49 -0300

    sub-consultas

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1109,3 +1109,73 @@ WHERE  e.job_id = j.job_id;
 
 
 --********** SUB-CONSULTAS **********--
+
+--Sub-Consultas Single-Row
+
+/*Quais empregados possuem o salario maior do
+que o a média de salarios?*/
+SELECT
+    first_name,
+    last_name,
+    job_id,
+    salary
+FROM
+    employees
+WHERE
+    salary > (
+        SELECT
+            AVG(nvl(salary, 0))
+        FROM
+            employees
+    );              
+
+-- Utilizando Sub-consultas na Cláusula HAVING
+-- Qual o id do departamento e o maior salario menor que a media? 
+SELECT
+    e1.department_id,
+    MAX(e1.salary)
+FROM
+    employees e1
+GROUP BY
+    e1.department_id
+HAVING
+    MAX(salary) < (
+        SELECT
+            AVG(e2.salary)
+        FROM
+            employees e2
+    );
+                                                  
+-- Erros utilizando Sub-consultas Single-Row
+SELECT
+    employee_id,
+    first_name,
+    last_name
+FROM
+    employees
+WHERE
+    salary = (
+        SELECT
+            AVG(nvl(salary, 0))
+        FROM
+            employees
+        GROUP BY
+            department_id
+    );
+
+-- O que ocorre quando a Sub-Consulta retorna nenhuma linha? NULL
+SELECT
+    employee_id,
+    first_name,
+    last_name
+FROM
+    employees
+WHERE
+    last_name = (
+        SELECT
+            last_name
+        FROM
+            employees
+        WHERE
+            last_name = 'Suzuki'
+    );
