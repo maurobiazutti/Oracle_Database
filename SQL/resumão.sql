d7f553423aa9423411c7f709a846ad5dd9c4c425
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-23 23:46:45 -0300

    exists

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1312,7 +1312,13 @@ WHERE  EXISTS
                WHERE d.department_id = 
                              e.department_id);
 
+-- Utilizando operador EXISTS
 
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  EXISTS (SELECT e.department_id
+               FROM employees e
+               WHERE d.department_id = e.department_id);
 
 
 
