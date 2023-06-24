f82d63329ae6bb8a9037452367b82f3c2196d688
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-23 23:47:32 -0300

    NOT EXISTS

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1320,6 +1320,14 @@ WHERE  EXISTS (SELECT e.department_id
                FROM employees e
                WHERE d.department_id = e.department_id);
 
+-- Utilizando operador NOT EXISTS
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  NOT EXISTS (SELECT e.department_id
+                   FROM employees e
+                   WHERE d.department_id = e.department_id);
+    
 
 
 
