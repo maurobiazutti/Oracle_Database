cb37ff0c63020cd0bd116502fec72d9782fecf73
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-23 23:48:43 -0300

    Sub-Consultas Correlacionadas

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1328,7 +1328,17 @@ WHERE  NOT EXISTS (SELECT e.department_id
                    FROM employees e
                    WHERE d.department_id = e.department_id);
     
-
+-- Utilizando Sub-Consultas Correlacionadas
+               
+SELECT e1.employee_id, e1.first_name, e1.last_name, e1.department_id, e1.salary
+FROM   employees e1
+WHERE  e1.salary >= (SELECT    TRUNC(AVG(NVL(salary,0)),0)
+                     FROM      employees e2
+                     WHERE     e1.department_id = e2.department_id);
+                   
+SELECT    TRUNC(AVG(NVL(salary,0)),0)
+FROM      employees e2
+WHERE     e2.department_id = 60;
 
 
 
