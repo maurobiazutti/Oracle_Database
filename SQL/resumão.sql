c4b4aed50416bab4e47a6c5464260e88533478af
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-23 23:46:05 -0300

    operador EXISTS e NOT EXISTS

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1302,6 +1302,16 @@ WHERE
     );
 
 
+-- Utilizando operador EXISTS
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  EXISTS
+             (SELECT e.department_id
+               FROM   employees e
+               WHERE d.department_id = 
+                             e.department_id);
+
 
 
 
