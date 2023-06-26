523ca7bf7edf7eec6db8c8878bed20aec0e51053
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-26 11:36:52 -0300

    DML - data/hota

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1425,3 +1425,28 @@ department_name)
 VALUES (302, 'IOT');
 
 COMMIT;
+
+
+-- Inserindo valores especiais retornados de Funções
+INSERT INTO employees 
+           (employee_id, first_name, last_name, email, 
+            phone_number, hire_date, job_id, salary,
+            commission_pct, manager_id, department_id)
+       VALUES (207, 'Rock', 'Balboa', 'DROCK', 
+               '525.342.237', SYSDATE, 'IT_PROG', 7000,
+               NULL, 103, 60);
+
+-- Insert utilizando data e hora específicas
+INSERT INTO employees 
+           (employee_id, first_name, last_name, email, 
+            phone_number, hire_date, job_id, salary,
+            commission_pct, manager_id, department_id)
+       VALUES (208, 'Vito', 'Corleone', 'VCORL', 
+               '525.342.237', TO_DATE('11/02/2020', 'DD/MM/YYYY'), 'IT_PROG', 20000,
+               NULL, 103, 60);
+               
+SELECT *
+FROM   employees
+ORDER BY employee_id DESC;
+
+COMMIT;
