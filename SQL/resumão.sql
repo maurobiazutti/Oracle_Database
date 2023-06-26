1dd3f0da0ae6cf5cddd3305858098bc078e9d978
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-26 11:40:34 -0300

    DML - Savepoint

--- SQL/resumão.sql
+++ SQL/resumão.sql
@@ -1525,3 +1525,32 @@ WHERE employee_id = 208;
 
 COMMIT;
 
+-- Utilizando o comando COMMIT
+
+-- Utilizando Savepoint
+DELETE FROM employees
+WHERE employee_id IN (207,208);
+
+COMMIT;
+
+INSERT INTO employees 
+           (employee_id, first_name, last_name, email, 
+            phone_number, hire_date, job_id, salary,
+            commission_pct, manager_id, department_id)
+       VALUES (207, 'Rock', 'Balboa', 'DROCK', 
+               '525.342.237', SYSDATE, 'IT_PROG', 7000,
+               NULL, 103, 60);
+
+SAVEPOINT A;
+
+INSERT INTO employees 
+           (employee_id, first_name, last_name, email, 
+            phone_number, hire_date, job_id, salary,
+            commission_pct, manager_id, department_id)
+       VALUES (208, 'Vito', 'Corleone', 'VCORL', 
+               '525.342.237', TO_DATE('11/02/2020', 'DD/MM/YYYY'), 'IT_PROG', 20000,
+               NULL, 103, 60);
+               
+ROLlBACK TO SAVEPOINT  A;
+
+COMMIT; 
