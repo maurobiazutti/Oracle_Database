b84255d9905ac939a20401e96130c67715285124
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-27 19:23:51 -0300

    03_exer

---
+++ Exercicios/03_exer.sql
@@ -0,0 +1,3 @@
+/*Crie uma consulta com todos os nomes de alunos matriculados no ano 
+de 2018, os nomes das disciplinas em que estão matriculados, 
+a cidade onde moram e os nomes dos cursos das disciplinas.*/
