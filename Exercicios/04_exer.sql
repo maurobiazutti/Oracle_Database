38266878022f2b6cbbfb68a8e7ed6fd709b0cf99
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-29 11:58:52 -0300

    res_04

--- Exercicios/04_exer.sql
+++ Exercicios/04_exer.sql
@@ -1,2 +1,21 @@
 /*Crie uma consulta que mostre todos os alunos de Palmas que cursam alguma 
 disciplina de Programação de Banco de dados no ano de 2018.*/
+
+--Minha Solução
+select
+    a.aluno_nome,
+    a.aluno_cidade,
+    d.disc_nome,
+    m.ano
+from
+    aluno a,
+    disciplina d,
+    matricula m
+where
+     (a.aluno_numero = m.aluno_numero)
+     and (d.disc_codigo = m.disc_codigo)
+     and (a.aluno_cidade = 'Palmas') 
+     and (d.disc_nome = 'Banco de dados')
+     and (m.ano = '2018');
+     
+--Exercicio não tem solução proposta
