bea7771accd9790026bd88679503f2bb0b6040eb
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-29 09:07:39 -0300

    res_02

--- Exercicios/02_exer.sql
+++ Exercicios/02_exer.sql
@@ -2,8 +2,24 @@
 CST Sistemas para Internet, no ano de 2018, e os nomes das 
 disciplinas pelas quais são responsáveis.*/
 
---nomes dos professores do curso de CST Sistemas para Internet, no ano de 2018
---nomes das disciplinas pelas quais são responsáveis
+-- Minha Resposta 
+select
+    p.prof_nome,
+    d.curso_nome,
+    d.disc_nome,
+    pd.ano
+from
+    professor p,
+    disciplina d,
+    professor_disciplina pd
+    where
+    (p.prof_numero = pd.prof_numero)
+    and (d.disc_codigo = pd.disc_codigo)
+    and (d.curso_nome = 'CST Sistemas para Internet')
+    and (pd.ano = '2018');
+    
+
+--Resposta proposta pelo exercicio   
 SELECT 
     p.prof_nome, 
     d.disc_nome, 
@@ -18,4 +34,4 @@ WHERE
     AND d.disc_codigo = pd.disc_codigo
     AND d.disc_codigo = m.disc_codigo
     AND d.curso_nome = 'CST Sistemas para Internet'
-    AND m.ano = 2018;
+    AND m.ano = '2018';
