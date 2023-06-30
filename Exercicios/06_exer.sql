f4ea4ef9fc7029a1c231c4705e566303b3627d52
maurobiazutti <mauro_biazutti@hotmail.com>
2023-06-30 13:27:08 -0300

    resolvendo

--- Exercicios/06_exer.sql
+++ Exercicios/06_exer.sql
@@ -1,1 +1,22 @@
 /*Crie uma consulta que mostre o nome dos professores e a média de aulas que ministram.*/
+
+
+-- Professores e suas diciplinas
+SELECT * FROM professor;
+SELECT * FROM professor_disciplina;
+SELECT * FROM disciplina;
+SELECT * FROM matricula;
+
+select 
+    p.prof_numero,
+    p.prof_nome,
+    d.disc_codigo,
+    d.disc_nome,
+    d.qtd_aulas
+from
+    professor p,
+    disciplina d,
+    professor_disciplina pd
+where
+    (p.prof_numero = pd.prof_numero)
+    and (d.disc_codigo = pd.disc_codigo);
