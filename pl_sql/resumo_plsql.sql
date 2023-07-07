756aeddb6042ead5185e516dca47583f86223807
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-07 18:22:04 -0300

    loop

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -358,3 +358,18 @@ FROM   employees
 GROUP BY department_id;
 
 
+-- LOOP Básico
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(5) := 1;
+  vLimite  NUMBER(5) := &pLimite;
+BEGIN
+
+LOOP 
+  DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(vNumero));
+  EXIT WHEN vNumero = vLimite;
+  vNumero := vNumero + 1;
+END LOOP;
+END;
