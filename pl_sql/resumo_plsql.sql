9d02f58188b91762272ffd2bc83bef5b3d1db03b
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-10 21:05:59 -0300

    procedure_01

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -432,3 +432,49 @@ BEGIN
 -- CÓDIGO DA PROCEDURE
 
 END;
+
+--EX - 1 - Inserindo novos dados na tabela
+CREATE OR REPLACE PROCEDURE PRC_INSERE_EMPREGADO
+-- variaveis que vão receber os parametros
+  (pfirst_name    IN VARCHAR2,
+   plast_name     IN VARCHAR2,
+   pemail         IN VARCHAR2,
+   pphone_number  IN VARCHAR2,
+   phire_date     IN DATE DEFAULT SYSDATE,
+   pjob_id        IN VARCHAR2,
+   pSALARY        IN NUMBER,
+   pCOMMICION_PCT IN NUMBER,
+   pMANAGER_ID    IN NUMBER,
+   pDEPARTMENT_ID IN NUMBER)
+IS 
+
+BEGIN
+-- inserindo os parametros recebidos
+  INSERT INTO employees (
+    employee_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    hire_date,
+    job_id,
+    salary,
+    commission_pct,
+    manager_id,
+    department_id )
+  VALUES (
+    employees_seq.nextval,
+    pfirst_name,
+    plast_name,
+    pemail,
+    pphone_number,
+    phire_date,
+    pjob_id,
+    psalary,
+    pcommicion_pct,
+    pmanager_id,
+    pdepartment_id );
+EXCEPTION
+  WHEN OTHERS THEN
+     RAISE_APPLICATION_ERROR(-20001, 'Erro Oracle ' || SQLCODE || SQLERRM);
+END;
