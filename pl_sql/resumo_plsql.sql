50f66236c63dc2035ad3871a0881c5ee71afa6c8
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-10 21:07:39 -0300

    procedure_03

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -500,3 +500,20 @@ SELECT *
 FROM   employees
 WHERE  first_name = 'Greg' AND
        last_name = 'Lake';
+
+--EX - 2 - Aumento de Salario
+CREATE OR REPLACE PROCEDURE PRC_AUMENTA_SALARIO_EMPREGADO
+  (pemployee_id   IN NUMBER,
+   ppercentual    IN NUMBER)
+IS
+  -- Nenhuma Variavel declarada
+BEGIN
+  UPDATE employees 
+  SET salary = salary * (1 + ppercentual / 100)
+  WHERE employee_id = pemployee_id;
+
+EXCEPTION
+  WHEN OTHERS 
+  THEN
+     RAISE_APPLICATION_ERROR(-20001, 'Erro Oracle: ' || SQLCODE || ' - ' || SQLERRM);
+END;
