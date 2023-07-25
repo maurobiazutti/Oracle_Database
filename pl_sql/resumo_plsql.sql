358aa22c43651cd45ace451b35eab7307fc1ec7c
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-24 22:32:36 -0300

    for

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -725,3 +725,16 @@ FOR i IN 1..8 LOOP
 END LOOP;
 DBMS_OUTPUT.PUT_LINE('Total Final =  ' || to_char(vTotal,'99G999G999G999G999G999G999G999D99'));
 END;
+
+-- FOR LOOP
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vInicio  INTEGER(3) := 1;
+  vFim     NUMBER(38) := &pLimite;
+BEGIN
+FOR i IN vinicio..vfim  LOOP
+  DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(i) );
+END LOOP;
+END;
