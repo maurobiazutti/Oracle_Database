668d07af442c8106a1e655b70d5ba2670bde279e
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-25 01:25:29 -0300

    loop

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -666,3 +666,20 @@ BEGIN
                          employees_record.phone_number || ' - ' ||
                          LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
 END;
+
+
+-- LOOP Básico
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(5) := 1;
+  vLimite  NUMBER(5) := &pLimite;
+BEGIN
+
+LOOP 
+  DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(vNumero));
+  EXIT WHEN vNumero = vLimite;
+  vNumero := vNumero + 1;
+END LOOP;
+END;
