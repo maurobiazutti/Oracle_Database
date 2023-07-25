f2712c901f9f1210808ac3cbce5d5f66656c056b
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-24 22:33:35 -0300

    while

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -738,3 +738,17 @@ FOR i IN vinicio..vfim  LOOP
   DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(i) );
 END LOOP;
 END;
+
+-- WHILE LOOP
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(38) :=  1;
+  vLimite  NUMBER(38) := &pLimite;
+BEGIN
+WHILE  vNumero <= vLimite LOOP
+    DBMS_OUTPUT.PUT_LINE('Numero =  ' || to_char(vNumero));
+    vNumero := vNUmero + 1;
+END LOOP;
+END;
