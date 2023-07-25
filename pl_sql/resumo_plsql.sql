bc6fc8373798b2879c2a15bd405224608605f8db
maurobiazutti <mauro_biazutti@hotmail.com>
2023-07-24 22:34:52 -0300

    refatoração

--- pl_sql/resumo_plsql.sql
+++ pl_sql/resumo_plsql.sql
@@ -726,17 +726,6 @@ END LOOP;
 DBMS_OUTPUT.PUT_LINE('Total Final =  ' || to_char(vTotal,'99G999G999G999G999G999G999G999D99'));
 END;
 
--- FOR LOOP
-SET SERVEROUTPUT ON
-ACCEPT pLimite PROMPT 'Digite o valor do limite: '
-DECLARE
-  vInicio  INTEGER(3) := 1;
-  vFim     NUMBER(38) := &pLimite;
-BEGIN
-FOR i IN vinicio..vfim  LOOP
-  DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(i) );
-END LOOP;
-END;
 
 -- WHILE LOOP
 SET SERVEROUTPUT ON
@@ -750,3 +739,16 @@ WHILE  vNumero <= vLimite LOOP
     vNumero := vNUmero + 1;
 END LOOP;
 END;
+
+-- FOR LOOP
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vInicio  INTEGER(3) := 1;
+  vFim     NUMBER(38) := &pLimite;
+BEGIN
+FOR i IN vinicio..vfim  LOOP
+  DBMS_OUTPUT.PUT_LINE('Numero = ' || to_char(i) );
+END LOOP;
+END;
+
